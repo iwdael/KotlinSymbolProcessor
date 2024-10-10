9574673bcbd57ef9ebd68a1aabddda3a616a4c1d
iwdael <iwdael@outlook.com>
2024-10-10 16:50:01 +0800

    fix2 KSPType.asTypeName

--- symbolparser/src/main/java/com/iwdael/kotlinsymbolprocessor/KSPTypeExtension.kt
+++ symbolparser/src/main/java/com/iwdael/kotlinsymbolprocessor/KSPTypeExtension.kt
@@ -3,6 +3,7 @@ package com.iwdael.kotlinsymbolprocessor
 import com.squareup.kotlinpoet.ParameterizedTypeName.Companion.parameterizedBy
 import com.squareup.kotlinpoet.TypeName
 import com.squareup.kotlinpoet.ksp.toClassName
+import com.squareup.kotlinpoet.ksp.toTypeName
 
 /**
  * @author iwdael
@@ -14,7 +15,11 @@ fun KSPType.asTypeName(): TypeName {
         if (types.isEmpty()) {
             ksp.resolve().toClassName().copy(ksp.resolve().isMarkedNullable)
         } else {
-            ksp.resolve().toClassName().parameterizedBy(types.map { it!!.asTypeName() })
+            ksp.resolve().toClassName()
+                .let {
+                    it.copy(ksp.resolve().isMarkedNullable, it.annotations, it.tags)
+                }
+                .parameterizedBy(types.map { it!!.asTypeName() })
         }
     } catch (e: Exception) {
         throw RuntimeException(e)
